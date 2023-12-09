5f74d7006cd1d1880a8a8c314a9a2586b1c6743a
DIOR <diertojiev@yandex.ru>
2023-12-09 22:19:05 +0300

    Улучшенная валидация и использование jquery validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 "use strict"
 
 $(document).ready(function (){
+
+    //маска для номера
     $(".form__phone").mask("+7 (999) 999-99-99")
 
-    //modal
+    //модальные окна
     $('[data-modal=call]').on('click', function() {
         $('.overlay, #call').fadeIn();
         $("body").toggleClass('modal-open');
@@ -18,4 +20,37 @@ $(document).ready(function (){
         $('.overlay, #call, #request').fadeOut();
         $('body').removeClass('modal-open')
     })
+
+    //валидация окон
+    function validateForms(item){
+        $(item).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                phone: "required",
+            },
+
+            messages: {
+                name: {
+                    required: 'Введите ваше имя',
+                    minlength: jQuery.validator.format('Введите минимум {0} символа'),
+                },
+                email: {
+                    required: 'Введите вашу электронную почту',
+                    email: "Неправильно введена почта"
+                },
+                phone: 'Введите ваш номер телефона'
+            }
+        });
+    }
+
+    validateForms('#call form');
+    validateForms('#request form');
+    
 });
